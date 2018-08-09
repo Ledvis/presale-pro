4472b19e206ffa4e91eb89f1a97d106cd60ce475
Zhenya <fedyai.eugen@gmail.com>
2018-08-09 18:28:05 +0300

    Complete tablet view version

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,12 +1,9 @@
 import Swiper from 'swiper';
 
 export default function init() {
-  const swiper = new Swiper('.swiper-container', {
-    slideToClickedSlide: 'true',
+  const timelineSwiper = new Swiper('.timeline__wrapper', {
     grabCursor: true,
     centeredSlides: true,
-    slidesPerView: 1,
-    spaceBetween: 30,
     keyboard: {
       enabled: true,
     },
@@ -14,6 +11,71 @@ export default function init() {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    breakpoints: {
+      // when window width is <= 767px
+      767: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      // when window width is <= 1169px
+      1169: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
   });
-  // swiper.slideTo(2, 1000, false);
+  timelineSwiper.slideTo(2, 1000, false);
+
+  const teamSwiper = new Swiper('.team__wrapper', {
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+    breakpoints: {
+      // when window width is <= 767px
+      767: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      // when window width is <= 1169px
+      1169: {
+        slidesPerView: 2,
+        spaceBetween: 55,
+      },
+    },
+  });
+  teamSwiper.slideTo(1, 1000, false);
+
+  const pressSwiper = new Swiper('.press__wrapper', {
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    loop: true,
+    slidesPerView: 1,
+    breakpoints: {
+      // when window width is <= 767px
+      767: {
+        spaceBetween: 30,
+      },
+      // when window width is <= 1169px
+      1169: {
+        spaceBetween: 100,
+      },
+    },
+  });
+  pressSwiper.slideTo(2, 1000, false);
 }
