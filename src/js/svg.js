d449f827cd6cbd769bbef2cabbe4a517cd52fa62
Zhenya <fedyai.eugen@gmail.com>
2018-08-27 16:21:27 +0300

    Add final animations

--- src/js/svg.js
+++ src/js/svg.js
@@ -1,13 +1,15 @@
 require('waypoints/lib/noframework.waypoints.min.js');
 
 export default function () {
-  const turbineSvg = document.querySelector('#turbine');
+  const svgImages = document.querySelectorAll('.animate');
 
-  const waypoint = new Waypoint({
-    element: document.querySelector('#turbine'),
-    handler() {
-      turbineSvg.classList.add('support__img--active');
-    },
-    offset: '25%',
+  svgImages.forEach((svg) => {
+    const waypoint = new Waypoint({
+      element: svg,
+      handler() {
+        svg.classList.add('animate--active');
+      },
+      offset: '25%',
+    });
   });
 }
